9afd3087ee7137cb4f4bc7304de1a6a97dc8285d
iorehovski <iorehovski@gmail.com>
2017-09-17 19:24:06 +0300

    GUESSING GAME

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+    	var this.min = 0;
+    	var this.max = 0;
+        var this.middle = 0;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round(this.min + (this.max -this.min)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
